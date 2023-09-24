0acacff432691bbafba6f9cac7bd11ef1aa41513
rir360 <rezowanislamrizvy7@gmail.com>
2023-09-24 23:08:16 +0600

    hello route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,9 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 router.post("/api/testapi", async (req, res) => {
-    const name = req.body
-    console.log(req.body);
-    res.status(401).send(name)
+  const name = req.body
+  console.log(req.body);
+  res.status(401).send(name)
+})
+
+router.get("/api/hello", async (req, res) => {
+  const code = 200;
+  res.status(code).send({
+    code, message: "Hello World"
+  })
 })
 
 module.exports = router
